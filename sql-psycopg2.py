44e3bf5f62db5d180e9cebbddece66861bce3c9a
joeyo991 <joeyosull@gmail.com>
2022-04-28 15:17:17 +0000

    Running Basic Queries

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -22,7 +22,7 @@ cursor = connection.cursor()
 # Query 5 - select only the albums with "ArtistId" #51 on the "Album" table
 # cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51])
 
-# Query 6 - select all tracks where the composer is "Queen" from the "Track" 
+# Query 6 - select all tracks where the composer is "Queen" from the "Track"
 # table
 cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
 
